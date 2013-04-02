6ddbeb7f7de2faf189f57ed84810c216244ce7e9
KONRAD Frederic <fred.konrad@greensocs.com>
2013-04-02 15:45:47 +0200

    FindTLM: fix error handling.
    This fix the error handling, and add some hints for finding TLM headers.
    
    Signed-off-by: KONRAD Frederic <fred.konrad@greensocs.com>

--- FindTLM.cmake
+++ FindTLM.cmake
@@ -36,23 +36,36 @@ message(STATUS "Searching for TLM")
 SET(_TLM_HINTS
   "[HKEY_LOCAL_MACHINE\\SOFTWARE\\TLM\\2.2;TLMHome]/include"
   $ENV{TLM_HOME}
+  ${SYSTEMC_PREFIX}/include
+  ${SYSTEMC_PREFIX}/lib
+  ${SYSTEMC_PREFIX}/lib-linux
+  ${SYSTEMC_PREFIX}/lib-linux64
+  ${SYSTEMC_PREFIX}/lib-macos
+  $ENV{SYSTEMC_PREFIX}/include
+  $ENV{SYSTEMC_PREFIX}/lib
+  $ENV{SYSTEMC_PREFIX}/lib-linux
+  $ENV{SYSTEMC_PREFIX}/lib-linux64
+  $ENV{SYSTEMC_PREFIX}/lib-macos
+  ${CMAKE_INSTALL_PREFIX}/include
+  ${CMAKE_INSTALL_PREFIX}/lib
+  ${CMAKE_INSTALL_PREFIX}/lib-linux
+  ${CMAKE_INSTALL_PREFIX}/lib-linux64
+  ${CMAKE_INSTALL_PREFIX}/lib-macos
   )
 # Hard-coded guesses should still go in PATHS. This ensures that the user
 # environment can always override hard guesses.
 SET(_TLM_PATHS
   /usr/include/TLM
+  /usr/include/systemc
+  /usr/lib
+  /usr/lib-linux
+  /usr/lib-linux64
+  /usr/lib-macos
+  /usr/local/lib
+  /usr/local/lib-linux
+  /usr/local/lib-linux64
+  /usr/local/lib-macos
   )
-
-
-
-set(TLM_MAJOR 2)
-set(TLM_MINOR 0)
-set(TLM_VERSION ${TLM_MAJOR}.${TLM_MINOR})
-
-message(STATUS "TLM_VERSION = ${TLM_VERSION}")
-
-set(TLM_FOUND TRUE)
-
 
 FIND_PATH(TLM_INCLUDE_DIRS
   NAMES tlm.h
@@ -60,3 +73,13 @@ FIND_PATH(TLM_INCLUDE_DIRS
   PATHS ${_TLM_PATHS}
   PATH_SUFFIXES include/tlm
 )
+
+EXEC_PROGRAM("ls ${TLM_INCLUDE_DIRS}/tlm.h"
+             RETURN_VALUE ret)
+
+if("${ret}" MATCHES "0")
+  set(TLM_FOUND TRUE)
+endif("${ret}" MATCHES "0")
+
+message(STATUS "TLM library = ${TLM_INCLUDE_DIRS}/tlm.h")
+
