30cdadcf62d460b757d3ff5dc3da8e7422a8847c
KONRAD Frederic <fred.konrad@greensocs.com>
2014-11-11 11:15:05 +0100

    serialsocket: fix license according to the LICENSE file.
    This fixes the license headers according to the license file.
    
    Signed-off-by: KONRAD Frederic <fred.konrad@greensocs.com>

--- greenserialsocket/include/greenserialsocket/tlm_serial_phase.h
+++ greenserialsocket/include/greenserialsocket/tlm_serial_phase.h
@@ -1,36 +1,44 @@
-// LICENSETEXT
-//
-//   Copyright (C) 2008 : GreenSocs Ltd
-//       http://www.greensocs.com/ , email: info@greensocs.com
-//
-//   Developed by :
-//
-//   Manish Aggarwal, Ruchir Bharti
-//    GreenSocs Ltd
-//
-//
-//   This program is free software.
-//
-//   If you have no applicable agreement with GreenSocs Ltd, this software
-//   is licensed to you, and you can redistribute it and/or modify
-//   it under the terms of the GNU General Public License as published by
-//   the Free Software Foundation; either version 2 of the License, or
-//   (at your option) any later version.
-//
-//   If you have a applicable agreement with GreenSocs Ltd, the terms of that
-//   agreement prevail.
-//
-//   This program is distributed in the hope that it will be useful,
-//   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//   GNU General Public License for more details.
-//
-//   You should have received a copy of the GNU General Public License
-//   along with this program; if not, write to the Free Software
-//   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
-//   02110-1301  USA
-//
-// ENDLICENSETEXT
+/*
+ * tlm_serial_phase.h
+ *
+ * Copyright (C) 2014, GreenSocs Ltd.
+ *
+ * Developped by Manish Aggarwal, Ruchir Bharti
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ * Linking GreenSocs code, statically or dynamically with other modules
+ * is making a combined work based on GreenSocs code. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole
+ * combination.
+ *
+ * In addition, as a special exception, the copyright holders, GreenSocs
+ * Ltd, give you permission to combine GreenSocs code with free software
+ * programs or libraries that are released under the GNU LGPL, under the
+ * OSCI license, under the OCP TLM Kit Research License Agreement or
+ * under the OVP evaluation license.You may copy and distribute such a
+ * system following the terms of the GNU GPL and the licenses of the
+ * other code concerned.
+ *
+ * Note that people who make modified versions of GreenSocs code are not
+ * obligated to grant this special exception for their modified versions;
+ * it is their choice whether to do so. The GNU General Public License
+ * gives permission to release a modified version without this exception;
+ * this exception also makes it possible to release a modified version
+ * which carries forward this exception.
+ *
+ */
 
 #ifndef __TLM_SERIAL_PHASE_H__
 #define __TLM_SERIAL_PHASE_H__
