2d4e7b7f3f69faa3d1cb0cac8ab509d15a01e0bd
Guillaume Delbergue <guillaume.delbergue@greensocs.com>
2015-04-09 13:58:23 +0200

    cmake: improve FindTLM.
    Really provide TLM Version
    Clean PATHS and HINTS
    Prefer tlm to tlm.h (backward compatibility)
    Cross-platform (remove EXEC_PROGRAM)
    
    Signed-off-by: Guillaume Delbergue <guillaume.delbergue@greensocs.com>

--- FindTLM.cmake
+++ FindTLM.cmake
@@ -1,14 +1,12 @@
-# - Find TLM
+# Find TLM
 # This module finds if TLM is installed and determines where the
 # include files and libraries are. This code sets the following
-# variables: (from kernel/sc_ver.h)
+# variables: (from tlm_version.h)
 #
-#  TLM_VERSION_STRING     = Version of the package found, eg. "2.2.0"
 #  TLM_VERSION_MAJOR      = The major version of the package found.
 #  TLM_VERSION_MINOR      = The minor version of the package found.
 #  TLM_VERSION_PATCH      = The patch version of the package found.
-#  TLM_VERSION_DATE       = The date of release (from TLM_VERSION)
-#  TLM_VERSION            = This is set to: $major.$minor.$patch
+#  TLM_VERSION            = The full version string of the package found.
 #
 # The minimum required version of TLM can be specified using the
 # standard CMake syntax, e.g. FIND_PACKAGE(TLM 2.2)
@@ -17,69 +15,61 @@
 #
 #  TLM_FOUND                    - TRUE if all components are found.
 #  TLM_INCLUDE_DIRS             - Full paths to all include dirs.
-#  TLM_LIBRARIES                - Full paths to all libraries.
-#  TLM_<component>_FOUND        - TRUE if <component> is found.
 #
 # Example Usages:
 #  FIND_PACKAGE(TLM)
-#  FIND_PACKAGE(TLM 2.3)
-#
 
 #=============================================================================
-# Copyright 2012 GreenSocs
-#
+# Copyright 2015 GreenSocs
 #=============================================================================
 
-message(STATUS "Searching for TLM")
+set(_TLM_HINTS
+    ${TLM_PREFIX}
+    ${SYSTEMC_PREFIX}/include
+    $ENV{TLM_HOME}
+    $ENV{SYSTEMC_HOME}/include)
 
-# The HINTS option should only be used for values computed from the system.
-SET(_TLM_HINTS
-  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\TLM\\2.2;TLMHome]/include"
-  $ENV{TLM_HOME}
-  ${SYSTEMC_PREFIX}/include
-  ${SYSTEMC_PREFIX}/lib
-  ${SYSTEMC_PREFIX}/lib-linux
-  ${SYSTEMC_PREFIX}/lib-linux64
-  ${SYSTEMC_PREFIX}/lib-macos
-  $ENV{SYSTEMC_PREFIX}/include
-  $ENV{SYSTEMC_PREFIX}/lib
-  $ENV{SYSTEMC_PREFIX}/lib-linux
-  $ENV{SYSTEMC_PREFIX}/lib-linux64
-  $ENV{SYSTEMC_PREFIX}/lib-macos
-  ${CMAKE_INSTALL_PREFIX}/include
-  ${CMAKE_INSTALL_PREFIX}/lib
-  ${CMAKE_INSTALL_PREFIX}/lib-linux
-  ${CMAKE_INSTALL_PREFIX}/lib-linux64
-  ${CMAKE_INSTALL_PREFIX}/lib-macos
-  )
-# Hard-coded guesses should still go in PATHS. This ensures that the user
-# environment can always override hard guesses.
-SET(_TLM_PATHS
-  /usr/include/TLM
-  /usr/include/systemc
-  /usr/lib
-  /usr/lib-linux
-  /usr/lib-linux64
-  /usr/lib-macos
-  /usr/local/lib
-  /usr/local/lib-linux
-  /usr/local/lib-linux64
-  /usr/local/lib-macos
-  )
+set(_TLM_PATHS
+    /usr/local/include/tlm
+    /usr/local/include/systemc
+    /usr/include/tlm
+    /usr/include/systemc)
 
-FIND_PATH(TLM_INCLUDE_DIRS
-  NAMES tlm.h
-  HINTS ${_TLM_HINTS}
-  PATHS ${_TLM_PATHS}
-  PATH_SUFFIXES include/tlm
-)
+find_file(_TLM_VERSION_FILE
+          NAMES tlm_version.h
+          HINTS ${_TLM_HINTS}
+          PATHS ${_TLM_PATHS}
+          PATH_SUFFIXES tlm_h
+                        tlm/tlm_h
+                        tlm_core/tlm_2)
 
-EXEC_PROGRAM("ls ${TLM_INCLUDE_DIRS}/tlm.h"
-             RETURN_VALUE ret)
+if(EXISTS ${_TLM_VERSION_FILE})
+    file (READ ${_TLM_VERSION_FILE} _TLM_VERSION_FILE_CONTENTS)
 
-if("${ret}" MATCHES "0")
-  set(TLM_FOUND TRUE)
-endif("${ret}" MATCHES "0")
+    string (REGEX MATCH "TLM_VERSION_MAJOR[ \t]+([0-9]+)"
+            TLM_VERSION_MAJOR ${_TLM_VERSION_FILE_CONTENTS})
+    string (REGEX MATCH "([0-9]+)" TLM_VERSION_MAJOR
+            ${TLM_VERSION_MAJOR})
+    string (REGEX MATCH "TLM_VERSION_MINOR[ \t]+([0-9]+)"
+            TLM_VERSION_MINOR ${_TLM_VERSION_FILE_CONTENTS})
+    string (REGEX MATCH "([0-9]+)" TLM_VERSION_MINOR
+            ${TLM_VERSION_MINOR})
+    string (REGEX MATCH "TLM_VERSION_PATCH[ \t]+([0-9]+)"
+            TLM_VERSION_PATCH ${_TLM_VERSION_FILE_CONTENTS})
+    string (REGEX MATCH "([0-9]+)" TLM_VERSION_PATCH
+            ${TLM_VERSION_PATCH})
 
-message(STATUS "TLM library = ${TLM_INCLUDE_DIRS}/tlm.h")
+    set(TLM_VERSION "${TLM_VERSION_MAJOR}.${TLM_VERSION_MINOR}.${TLM_VERSION_PATCH}")
 
+    find_path(TLM_INCLUDE_DIRS
+              NAMES tlm tlm.h
+              HINTS ${_TLM_HINTS}
+              PATHS ${_TLM_PATHS}
+              PATH_SUFFIXES tlm)
+
+endif()
+
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(TLM
+                                  REQUIRED_VARS TLM_INCLUDE_DIRS
+                                  VERSION_VAR TLM_VERSION)
