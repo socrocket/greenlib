e1ef840409f13b3802f004141da1c95e96544038
KONRAD Frederic <fred.konrad@greensocs.com>
2015-09-15 15:29:23 +0200

    gsgpsocket: introduce tlm_b_if_wrapper.
    This introduces tlm_b_if_wrapper which is needed when we want more than one
    GenericSlaveSocket in the module.
    It allows to specify a callback instead of beeing stuck with the b_transact so
    the socket from where comes the transaction is known.
    
    Signed-off-by: KONRAD Frederic <fred.konrad@greensocs.com>

--- gsgpsocket/include/gsgpsocket/core/tlm_b_if.h
+++ gsgpsocket/include/gsgpsocket/core/tlm_b_if.h
@@ -27,9 +27,7 @@
 #include <boost/config.hpp> // needed for SystemC 2.1
 #include <systemc>
 
-
 namespace gs {
-
 
   template <class TRANSACTION>
   class tlm_b_if : public virtual sc_core::sc_interface
@@ -59,8 +57,43 @@ namespace gs {
   class tlm_td_multi_b_if : public virtual sc_core::sc_interface
   {
   public:
-    virtual void b_transact(TRANSACTION, sc_core::sc_time& time, unsigned int index) = 0;
+    virtual void b_transact(TRANSACTION, sc_core::sc_time& time,
+                            unsigned int index) = 0;
     virtual ~tlm_td_multi_b_if(){}
+  };
+
+  /*
+   * Allow to specify the b_transact function instead of beeing stuck with the
+   * b_transact name.
+   */
+  template<class MOD, class TRANSACTION>
+  class tlm_b_if_wrapper:
+    public gs::tlm_b_if<TRANSACTION>
+  {
+    public:
+      tlm_b_if_wrapper() {}
+      ~tlm_b_if_wrapper() {}
+
+      void register_b_transport(
+          void (MOD::*b_transact)(gs::gp::GenericSlaveAccessHandle), MOD *mod)
+      {
+        this->b_transport = b_transact;
+        this->mod = mod;
+      }
+
+    private:
+      void (MOD::*b_transport)(gs::gp::GenericSlaveAccessHandle);
+      MOD *mod;
+
+      void b_transact(gs::gp::GenericSlaveAccessHandle ah)
+      {
+        if (!b_transport || !mod)
+        {
+          SC_REPORT_ERROR(__FILE__, "No b_transport has been registered.");
+        }
+
+        (*mod.*b_transport)(ah);
+      }
   };
   
 } // end of namespace gs
